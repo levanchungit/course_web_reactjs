0bdf3c69fbc22b20ae7b4feaaacc75510d3e6d54
Chung Le Van <levanchung.it@gmail.com>
2023-11-07 21:21:24 +0700

    init repo

---
+++ src/features/Podcast/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+PodcastFeature.propTypes = {
+    
+};
+
+function PodcastFeature(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default PodcastFeature;
