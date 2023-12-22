dad04c6189dd77fe31da059c61892f9e8899a947
Chung Le Van <levanchung.it@gmail.com>
2023-12-22 20:22:56 +0700

    upd: fix maxWidth image frame layout content

--- src/components/Blog/Components/MarkDown.jsx
+++ src/components/Blog/Components/MarkDown.jsx
@@ -1,13 +1,37 @@
-/* eslint-disable */
 import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { dracula } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import rehypeSanitize from "rehype-sanitize";
+
+const removeParagraphTags = () => (tree) => {
+  tree.children.forEach((node) => {
+    if (
+      node.tagName === "p" &&
+      node.children.length === 1 &&
+      node.children[0].tagName === "img"
+    ) {
+      tree.children.splice(tree.children.indexOf(node), 1, node.children[0]);
+    }
+  });
+};
+
+const fixImages = () => (tree) => {
+  tree.children.forEach((node) => {
+    if (node.tagName === "img" || node.tagName === "pre") {
+      node.properties = node.properties || {};
+      node.properties.style = "max-width: 100%; height: auto;";
+    }
+  });
+};
+
+const rehypePlugins = [rehypeSanitize, removeParagraphTags, fixImages];
 
 export default function MarkDown({ markdown }) {
   return (
     <Markdown
       remarkPlugins={[remarkGfm]}
+      rehypePlugins={rehypePlugins}
       components={{
         code({ node, inline, className, children, ...props }) {
           const match = /language-(\w+)/.exec(className || "");
